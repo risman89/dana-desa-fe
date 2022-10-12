95d023d1bed18621fdc84fe822fdc895d259b42e
kadarisman <yuzarqadariesman@gmail.com>
2022-10-12 20:21:00 +0700

    add router.query

--- pages/admin/pendapatan/[id].js
+++ pages/admin/pendapatan/[id].js
@@ -1,7 +1,10 @@
 import { PendapatanProvider } from "src/context/PendapatanContext"
 import Edit from "@components/admin/pendapatan/Edit"
+import { useRouter } from 'next/router'
 
 const EditPendapatan = () => {
+    const router = useRouter()
+    const { id } = router.query
     return (
     <PendapatanProvider> 
         <Edit/>
