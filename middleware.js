cf9516f2dfcd76b280b36d7187950bb4f93bc364
kadarisman <yuzarqadariesman@gmail.com>
2022-10-12 23:09:47 +0700

    ubah url

--- middleware.js
+++ middleware.js
@@ -4,6 +4,7 @@ import { getToken } from "next-auth/jwt"
 export default async function middleware(req){
     const path = req.nextUrl.pathname;
     const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    console.log(session);
     
     if(path.startsWith('/admin') && !session){
         return NextResponse.redirect(new URL('/login', req.url))
