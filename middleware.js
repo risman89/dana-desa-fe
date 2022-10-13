73e98c6c351d4bf0879978759742567a0c827682
kadarisman <yuzarqadariesman@gmail.com>
2022-10-13 13:49:57 +0700

    tanpa session

--- middleware.js
+++ middleware.js
@@ -5,8 +5,8 @@ export default async function middleware(req){
     const path = req.nextUrl.pathname;
     const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
     // console.log(session);
-    
-    if(path.startsWith('/admin') && !session){
+    // 
+    if(path.startsWith('/admin')){
         return NextResponse.redirect(new URL('/login', req.url))
     }
 }
